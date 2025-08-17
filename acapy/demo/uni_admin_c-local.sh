5750f8b341a70b7297724a5095d294a3a99b3c85
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2025-08-17 23:55:17 +0600

    sh

---
+++ acapy/demo/uni_admin_c-local.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# this runs the University Admin Agent C example as a local instance of aca-py
+# you need to run a local von-network (in the von-network directory run "./manage start <your local ip> --logs")
+# ... and you need to install the local aca-py python libraries locally ("pip install -r ../requirements.txt -r ../requirements.indy.txt -r ../requirements.bbs.txt")
+
+# Check if ngrok tunnel is available
+NGROK_ENDPOINT=$(curl --silent localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+if [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]; then
+  echo "No ngrok tunnel found. Using localhost endpoint."
+  ENDPOINT="http://127.0.0.1:8120"
+else
+  echo "Using ngrok endpoint: $NGROK_ENDPOINT"
+  ENDPOINT=$NGROK_ENDPOINT
+fi
+
+# Pass the endpoint as an environment variable to the Python script
+ENDPOINT=$ENDPOINT PYTHONPATH=.. python3 -m runners.uni_admin_c $@
